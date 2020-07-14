a1e4096a5c3e7580ec4765b916ce13a812772650
Andrew Hartness <ahartness@amtdirect.com>
2020-07-14 06:47:51 -0400

    intial folder & file setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import Chess from 'react-chess';
 import './App.css';
 
-function App() {
+export const App: React.FC = () => {
+
+  const lineup = ['R@h1', 'P@f2', 'q@d8', 'R@a1', 'P@a2', 'P@c2', 'b@c8', 'p@d7', 'Q@d1', 'n@g8']
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chess 
+        pieces={lineup}
+      />
     </div>
   );
 }
